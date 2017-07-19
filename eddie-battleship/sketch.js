e8b9224625c26496fc3cf5f42f6455a8a3a1ee4d
eddieASC4 <rockspongebob12@gmail.com>
2017-07-19 11:55:03 -0400

    battleship fun

---
+++ eddie-battleship/sketch.js
@@ -0,0 +1,15 @@
+function setup (){
+    createCanvas(800,800);
+    background(0,0,255)
+}
+
+var value = 100;
+var recX = 0;
+var recY = 0;
+function draw(){
+    for(i=0;i<8;i++){
+        for (i = 0;i<8;i++){
+            
+        }
+    }
+}
